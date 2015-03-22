b400ab48e0b7d0c22f763f4aefcee102bdf5523c
QuentinImac-PC\Quentin <vecchioquentin@hotmail.fr>
2015-03-22 16:22:01 +0100

    Mise ? jour de la partie modulaire de l'algo

--- Client_Visual/Modelisation/Modelisation/main.cpp
+++ Client_Visual/Modelisation/Modelisation/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 	cout << "Projet L3 Recherche et modélisation 2015." << "Programme fait par Dylan Koby et Quentin Vecchio." << endl;
-	testUnitaireInfoGraphe("C:/Users/Quentin/Documents/projet-mod-lisation/test2.txt");
+	testUnitaireInfoGraphe("C:/Users/Quentin/Documents/projet-mod-lisation/test.txt");
 	//testUnitaireInfoGraphe("C:/Users/DK/Documents/Projet_Graphe/test.txt");
 	system("PAUSE");
 	return 0;
